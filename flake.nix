bf0bafa40c64cffb8e0507f79179efafdc681513
KOU050223 <kouhei20050223@icloud.com>
2026-05-10 17:24:29 +0900

    feat: pnpmとturboの設定ファイルを追加する

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Lang War development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pnpmPackage = pkgs.pnpm_10 or pkgs.pnpm;
+        goPackage = pkgs.go_1_26 or pkgs.go;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.git
+            pkgs.nodejs_24
+            pnpmPackage
+            goPackage
+          ];
+        };
+      });
+}
